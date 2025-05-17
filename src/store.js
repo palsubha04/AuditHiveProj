862e5bc99bbe48da45fb6d969d936d8697dcc1e1
Subhajit <suubhajiit@gmail.com>
2025-05-17 16:51:59 +0530

    updated store file

--- src/store.js
+++ src/store.js
@@ -6,6 +6,7 @@ import totalVsFlaggedTaxpayersReducer from './slice/totalVsFlaggedTaxpayersSlice
 import riskAnalysisByIndustryReducer from './slice/riskAnalysisByIndustrySlice';
 import employeeOnPayrollReducer from './slice/employeeOnPayrollSlice';
 import gstPayableVsRefundableReducer from './slice/gstPayableVsRefundableSlice';
+import riskAnomalyFrequencyReducer from './slice/riskAnomalyFrequencySlice';
 
 const store = configureStore({
   reducer: {
@@ -16,6 +17,7 @@ const store = configureStore({
     riskAnalysisByIndustry: riskAnalysisByIndustryReducer,
     employeeOnPayroll: employeeOnPayrollReducer,
     gstPayableVsRefundable: gstPayableVsRefundableReducer,
+    riskAnomalyFrequency: riskAnomalyFrequencyReducer,
   },
 });
 
