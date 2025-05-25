10ce59a07af2ec91e710bd3b6cc85ffe5d55c5ad
Soham Chakraborty <soham619cc@gmail.com>
2025-05-25 17:26:33 +0530

    ui changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,13 +3,50 @@ import Header from './Header';
 import Footer from './Footer';
 import Sidenav from './Sidenav';
 import './Layout.css';
+import { useLocation } from 'react-router-dom';
 
 function Layout({ children }) {
   const [isSidenavOpen, setIsSidenavOpen] = useState(true);
-
+  const location = useLocation();
   const toggleSidenav = () => {
     setIsSidenavOpen(!isSidenavOpen);
   };
+  let headerTitle;
+  let headerSubtitle;
+  if (location.pathname === '/compliance') {
+    headerTitle = 'Compliance';
+    headerSubtitle = '';
+  } else if (location.pathname === '/risk-assessment') {
+    headerTitle = 'Risk Assessment';
+    headerSubtitle = '';
+  } else if (location.pathname === '/upload-history') {
+    headerTitle = 'Upload History';
+    headerSubtitle = '';
+  } else if (location.pathname === '/risk-profiling') {
+    headerTitle = 'Risk Profiling';
+    headerSubtitle = '';
+  } else if (location.pathname === '/upload-sheets') {
+    headerTitle = 'Upload Sheets';
+    headerSubtitle = '';
+  } else if (location.pathname === '/recent-uploads') {
+    headerTitle = 'Recent Uploads';
+    headerSubtitle = '';
+  } else if (location.pathname === '/help-centre') {
+    headerTitle = 'Help Centre';
+    headerSubtitle = '';
+  } else if (location.pathname === '/contact-us') {
+    headerTitle = 'Contact Us';
+    headerSubtitle = '';
+  } else if (location.pathname === '/gst') {
+    headerTitle = 'GST Dashboard';
+    headerSubtitle = '';
+  } else if (location.pathname === '/swt') {
+    headerTitle = 'SWT Dashboard';
+    headerSubtitle = '';
+  } else if (location.pathname === '/cit') {
+    headerTitle = 'CIT Dashboard';
+    headerSubtitle = '';
+  }
 
   return (
     <div className="d-flex flex-column overflow-hidden" style={{ height: '100vh' }}>
@@ -18,10 +55,11 @@ function Layout({ children }) {
       </div>
       <div className='flex-1 d-flex flex-row overflow-hidden'>
         <div className='h-100'>
-          <Sidenav isOpen={isSidenavOpen} toggleSidenav={toggleSidenav}/>
+          <Sidenav isOpen={isSidenavOpen} toggleSidenav={toggleSidenav} />
         </div>
-        <div className='flex-1 p-4 overflow-auto'>
-          {children}
+        <div className='main-content'>
+          <div className='header-title-page'>{headerTitle} {headerSubtitle && <p>{headerSubtitle}</p>}</div>
+          <div>{children}</div>
         </div>
       </div>
       {/* <div className="">
